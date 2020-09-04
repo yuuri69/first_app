54332683b4ea8f7068caa73356d0196354fa4539
yuuri69 <70687538+yuuri69@users.noreply.github.com>
2020-09-04 14:59:29 +0900

    first commit

---
+++ app/controllers/application_controller.rb
@@ -0,0 +1,6 @@
+class ApplicationController < ActionController::Base
+
+  def index  # indexアクションを定義した
+  end
+end
+
---
+++ test/models/pist_test.rb
@@ -0,0 +1,7 @@
+require 'test_helper'
+
+class PistTest < ActiveSupport::TestCase
+  # test "the truth" do
+  #   assert true
+  # end
+end
